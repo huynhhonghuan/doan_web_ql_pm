0507f0c5ce7c9e14431890da4505438c8ba4d60c
huynhhonghuan <141419996+huynhhonghuan@users.noreply.github.com>
2023-12-08 15:16:49 +0700

    Delete webphim directory

--- webphim/resources/views/admin/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<script src="{{ asset('admin/plugins/jquery/jquery.min.js')}}"></script>
-<script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{ asset('admin/dist/js/adminlte.min.js')}}"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
-@yield('footer')
